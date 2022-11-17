17776169d1707f8cf15ee1aee003735666f452ad
adithya264 <adithya369varaprasad@gmail.com>
2022-11-16 20:04:32 -0600

    added

--- controllers/electronics.js
+++ controllers/electronics.js
@@ -119,3 +119,44 @@ exports.electronics_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };
+// Handle building the view for creating a electronics. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.electronics_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('electronicscreate', { title: 'electronics Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
+// Handle building the view for updating a electronics. 
+// query provides the id 
+exports.electronics_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await electronics.findById(req.query.id) 
+        res.render('electronicsupdate', { title: 'electronics Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.electronics_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await electronics.findById(req.query.id) 
+        res.render('electronicsdelete', { title: 'electronics Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
