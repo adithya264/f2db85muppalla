7475f50c71277ee46dec61a90d86b71f1a8eba1e
adithya264 <adithya369varaprasad@gmail.com>
2022-11-30 18:00:40 -0600

    added

--- routes/electronics.js
+++ routes/electronics.js
@@ -1,53 +1,31 @@
 var express = require('express');
-var electronics_controller = require('../controllers/electronics');
 var router = express.Router();
+const electronics_controllers= require('../controllers/electronics');
+// A little function to check if we have an authorized user and continue on 
 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+ 
 
+/* GET home page. */
+router.get('/',secured,electronics_controllers.electronics_view_all_Page);
+  
+/* GET detail electronics page */ 
+router.get('/detail', secured,electronics_controllers.electronics_view_one_Page); 
 
-// /* GET home page. */
+/* GET create electronics page */ 
+router.get('/create', secured,electronics_controllers.electronics_create_Page); 
 
-// router.get('/', function(req, res, next) {
+/* GET create update page */ 
+router.get('/update', secured,electronics_controllers.electronics_update_Page);
 
-//   res.render('electronics', { title: 'Search Results for electronics Class' });
-
-// });
-
-
-
-/* GET electronics */
-
-router.get('/', electronics_controller.electronics_view_all_Page );
-
-
-
-// GET request for one electronics.
-
-router.get('/electronicss/:id', electronics_controller.electronics_detail);
-
-
-
-/* GET detail electronics page */
-
-router.get('/detail', electronics_controller.electronics_view_one_Page);
-
-
-
-/* GET create electronics page */
-
-router.get('/create', electronics_controller.electronics_create_Page);
-
-
-
-/* GET create update page */
-
-router.get('/update', electronics_controller.electronics_update_Page);
-
-
-
-/* GET delete electronics page */
-
-router.get('/delete', electronics_controller.electronics_delete_Page);
-
-
+/* GET delete electronics page */ 
+router.get('/delete', secured,electronics_controllers.electronics_delete_Page); 
 
 module.exports = router;
